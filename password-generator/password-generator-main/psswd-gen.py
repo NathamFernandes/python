795033f59520db15c474d9fc7afc6fe39c19a256
Natham Fernandes <82012445+NathamFernandes@users.noreply.github.com>
2022-11-06 15:27:40 -0300

    Adição do respositório "password-generator"

---
+++ password-generator/password-generator-main/psswd-gen.py
@@ -0,0 +1,11 @@
+import random
+
+maiusculo = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+minusculo = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+numeros = ['1','2','3','4','5','6','7','8','9']
+especiais = ['@', '#', '!']
+senha = 
+tudo = maiusculo + minusculo + numeros + especiais
+
+for contador in range(10):
+    caracter = 
